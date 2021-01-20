ebb077d27b3cf044ce069b7797a7f6e7bf06b35b
Ramunas <rambek@gmail.com>
2021-01-20 13:46:05 +0200

    footer & menu image plugin

--- themes/keliaujantiems/footer.php
+++ themes/keliaujantiems/footer.php
@@ -1,4 +1,10 @@
-<footer>Footer</footer>
+<footer>
+    <img src="<?php echo get_template_directory_uri() . '/assets/img/keliaujantiems-logo-white.gif'; ?>" alt="Logo">
+    <p><a target="_blank" href=https://www.facebook.com/Keliaujantiems>Jungitės prie mūsų puslapio Facebooke.</a> Čia pirmieji sužinosite visas naujienas ir pasikeitimus Europos keliuose.</p>
+    <p>Jei turite ką pasakyti kelionių klausimais ar norite pasidalinti savo kelionių įspūdžiais rašykite el. paštu <a href="mailto:info@keliaujantiems.eu">info@keliaujantiems.eu</a></p>
+    <p>Visa medžiaga pateikta šiame puslapyje yra asmeninė nuosavybė. Kopijuoti, platinti be sutikimo DRAUDŽIAMA</p>
+    <p> Dizainas ir web sprendimas <a target="_blank" href=https://www.rambek.lt>rambek.lt</a> </p>
+</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
---
+++ themes/keliaujantiems/template-naujienos.php
@@ -0,0 +1,7 @@
+<?php /* Template Name: naujienos */ ?>
+<?php get_header() ?>
+<div class="news-block"> 
+    <h1 class="news-block-title"><?php the_title() ?></h1>
+
+</div>
+<?php get_footer() ?>
