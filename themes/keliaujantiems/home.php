3c985ae9e5e3174af5ac5c9c82afb455d72a9f14
Ramunas <rambek@gmail.com>
2021-01-20 15:59:13 +0200

    news block on main page

--- themes/keliaujantiems/home.php
+++ themes/keliaujantiems/home.php
@@ -5,6 +5,31 @@
             Pagalba planuojantiems keliones!
         </h1>
     </div>
+    <div class="main-page-news-block">
+        <?php 
+        $args = array(
+            'post_type' => 'post',
+            'post_status' => 'publish',
+            'category_name' => 'naujienos',
+            'posts_per_page' => 3,
+        );
+        $arr_posts = new WP_Query( $args );
+        ?>
+        <a href="<?php echo esc_url( home_url( '/naujienos/' ) ); ?>" class="main-page-news-block-title">Naujienos iš kelių</a>
+        <?php if ( $arr_posts->have_posts() ) :
+            while ( $arr_posts->have_posts() ) :
+            $arr_posts->the_post(); ?>
+                <div class="main-page-news-block">
+                <a href="<?php the_permalink(); ?>" class="main-page-news-post-title"><?php the_title() ?></a>
+                <span class="news-post-date"><?php echo get_the_date('F j, Y'); ?> </span>
+                <?php if ( has_post_thumbnail() ) : the_post_thumbnail('large');
+                endif; ?>
+                <?php the_excerpt(); ?>
+                <a class="main-page-news-block-read" href="<?php the_permalink(); ?>">Skaityti toliau</a>
+        </div>
+        <?php endwhile;
+        endif; ?>
+    </div>
     <div class="main-page-links-block">
         <a href="<?php echo esc_url( home_url( '/salys/' ) ); ?>" class="links-block-1">
             <img src="<?php echo get_template_directory_uri() . '/assets/img/map.jpg'; ?>" alt="map">
--- themes/keliaujantiems/template-naujienos.php
+++ themes/keliaujantiems/template-naujienos.php
@@ -1,7 +1,37 @@
 <?php /* Template Name: naujienos */ ?>
-<?php get_header() ?>
+<?php get_header(); 
+    
+    $args = array(
+    'post_type' => 'post',
+    'post_status' => 'publish',
+    'category_name' => 'naujienos',
+    'posts_per_page' => 5,
+);
+$arr_posts = new WP_Query( $args );
+?>
 <div class="news-block"> 
     <h1 class="news-block-title"><?php the_title() ?></h1>
-
+        <?php
+        if ( $arr_posts->have_posts() ) :
+ 
+    while ( $arr_posts->have_posts() ) :
+        $arr_posts->the_post();
+        ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <?php
+            if ( has_post_thumbnail() ) :
+                the_post_thumbnail('medium');
+            endif;
+            ?>
+            <div class="entry-content">
+                
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                <?php the_excerpt(); ?>
+            </div>
+        </article>
+        <?php
+    endwhile;
+endif;
+    ?>
 </div>
 <?php get_footer() ?>
