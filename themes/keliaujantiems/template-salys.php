4f95c0d69a130a3ac2dd5a8df732da8aa8be6cd7
Ramunas <rambek@gmail.com>
2021-01-21 13:26:28 +0200

    countries page

--- themes/keliaujantiems/template-salys.php
+++ themes/keliaujantiems/template-salys.php
@@ -1,38 +1,34 @@
 <?php /* Template Name: salys */ ?>
 <?php get_header() ?>
 <?php 
-    $args = array(
-    'post_type' => 'post',
-    'post_status' => 'publish',
-    'category_name' => 'salys',
-    'posts_per_page' => 5,
-);
+    $args = [
+    'post_type'         =>  'post',
+    'post_status'       =>  'publish',
+    'category_name'     =>  'salys',
+    'posts_per_page'    =>  -1,
+    'orderby'           =>  'title',
+    'order'             =>  'ASC'
+    ];
 $arr_posts = new WP_Query( $args );
 ?>
 
-<div class="countries-block"> 
-    <h1 class="countries-block-title"><?php the_title() ?></h1>
-    <?php
-        if ( $arr_posts->have_posts() ) :
- 
-    while ( $arr_posts->have_posts() ) :
-        $arr_posts->the_post();
-        ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <?php
-            if ( has_post_thumbnail() ) :
+<div class="countries-block">
+    <div class="countries-block-hero-photo">
+        <h1 class="countries-block-title"><?php the_title() ?></h1>
+    </div>
+    <div class="countries-list-block"> 
+    <?php if ( $arr_posts->have_posts() ) :
+            while ( $arr_posts->have_posts() ) :
+                $arr_posts->the_post(); ?>
+        
+            <a href="<?php the_permalink(); ?>" class="country-block">
+            <?php if ( has_post_thumbnail() ) :
                 the_post_thumbnail();
-            endif;
-            ?>
-            <div class="entry-content">
-                
-                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                <?php the_excerpt(); ?>
-            </div>
-        </article>
-        <?php
-    endwhile;
-endif;
-    ?>
+            endif; ?>
+            <h3><?php the_title(); ?></h3>
+            </a>
+        <?php endwhile;
+        endif; ?>
+    </div>
 </div>
 <?php get_footer() ?>
