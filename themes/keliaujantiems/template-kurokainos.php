8c3125d498494fa57edda53fe82e59155a059765
Ramunas <rambek@gmail.com>
2021-01-21 14:38:01 +0200

    fuel-price page

--- themes/keliaujantiems/template-kurokainos.php
+++ themes/keliaujantiems/template-kurokainos.php
@@ -1,6 +1,12 @@
 <?php /* Template Name: kurokainos */ ?>
 <?php get_header() ?>
 <div class="petrol-price-block"> 
-    <h1 class="petrol-price-title"><?php the_title() ?></h1>
-    <?php the_content() ?></div>
+    <div class=petrol-price-block-hero-photo>
+        <h1 class="petrol-price-title"><?php the_title() ?></h1>
+    </div>
+    <div class="petrol-price-content">
+        <?php the_content() ?>
+    </div>
+    
+</div>
 <?php get_footer() ?>
