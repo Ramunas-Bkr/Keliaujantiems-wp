fb219ac97b1704723dbb27cf8ab56b36bf6d0bff
Ramunas <rambek@gmail.com>
2021-01-23 21:39:14 +0200

    mobile menu

--- themes/keliaujantiems/assets/js/custom.js
+++ themes/keliaujantiems/assets/js/custom.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Header sticky START
 window.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('header');
     let sticky = header.offsetTop;
@@ -14,3 +15,37 @@ window.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('scroll', addClassWhenScrolling);
 });
+
+// Mobile menu START
+
+const showMenu = document.getElementById('switch-link');
+const mobileMenu = document.getElementById('mobile-menu');
+const closeMenu = document.getElementById('close-menu')
+
+document.onclick = function (event) {
+    let target = (event && event.target)
+    let display = 'none';
+    let switchDisplay = 'block';
+    document.body.classList.remove('modal_is_open');
+    while (target.parentNode) {
+        if (target == mobileMenu) {
+            display = 'block';
+            switchDisplay = 'none';
+            document.body.classList.add('modal_is_open');
+            break;
+        } else if (target == showMenu) {
+            display = 'block';
+            switchDisplay = 'none';
+            document.body.classList.add('modal_is_open');
+            break;
+        } else if (target == closeMenu) {
+            display = 'none';
+            switchDisplay = 'block';
+            document.body.classList.remove('modal_is_open');
+            break;
+        }
+        target = target.parentNode;
+    }
+    mobileMenu.style.display = display;
+    showMenu.style.display = switchDisplay;
+};
--- themes/keliaujantiems/header.php
+++ themes/keliaujantiems/header.php
@@ -8,20 +8,30 @@
 </head>
 <body>
 <header>
-<div class="header-links-area">
-<div class="logo-block">
-    <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-        <img src="<?php echo get_template_directory_uri() . '/assets/img/keliaujantiems-logo-new.gif'; ?>" alt="Logo">
-    </a>    
-</div>
-
-
-        <?php
-wp_nav_menu( ([ 
-    'theme_location' => 'my-custom-menu', 
-    'container' => false,
-    'menu_class' => 'custom-header-menu'
-])); 
-?>
+    <div class="header-links-area">
+        <div class="logo-block">
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                <img src="<?php echo get_template_directory_uri() . '/assets/img/keliaujantiems-logo-new.gif'; ?>" alt="Logo">
+            </a>    
+        </div>
+        <?php wp_nav_menu( ([ 
+            'theme_location' => 'my-custom-menu', 
+            'container' => false,
+            'menu_class' => 'custom-header-menu' ])); ?>
+    </div>
+    <div class="mobile-links-block">
+        <div id="switch-link" class="icon">
+            &#9776;
+        </div>
+        <div class="mobile-links" id="mobile-menu">
+            <span id="close-menu">&#10006;</span>
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                <img src="<?php echo get_template_directory_uri() . '/assets/img/keliaujantiems-logo-new.gif'; ?>" alt="Logo">
+            </a>    
+            <?php wp_nav_menu( ([
+                'theme_location' => 'my-custom-menu',
+                'container' => false,
+                'menu_class' => 'mobile-links-list' ]) ); ?>
+        </div>
     </div>
 </header>
